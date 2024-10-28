5f65f557dee6ae15e8836e1e53c0195608ccb080
Rajkumar Prajapati <120646423+Rajkumar-Prajapati@users.noreply.github.com>
2024-10-28 16:41:29 +0530

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,33 +1,41 @@
-
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
+// MongoDB URI from environment variables
+const MONGODB_URI = process.env.MONGODB_URI;
 
-interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
+console.log('MONGODB_URI', MONGODB_URI);
+
+// Define the cached object type
+interface Cached {
+  conn: Mongoose | null;  // Mongoose connection object or null
+  promise: Promise<Mongoose> | null; // Promise resolving to a Mongoose connection or null
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+// Create a global cached object if it doesn't exist
+const globalWithMongoose = global as typeof global & { mongoose: Cached };
 
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
-}
+// Initialize or retrieve the cached object
+const cached: Cached = globalWithMongoose.mongoose || { conn: null, promise: null };
 
+// Function to connect to the database
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  // If there is already a connection, return it
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  // Check if the MONGODB_URI is provided
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: ' Imaginefy', bufferCommands: false 
-    })
+  // Create a new connection promise if there is no cached promise
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently', // Database name
+    bufferCommands: false,
+  });
 
+  // Await the promise and store the connection
   cached.conn = await cached.promise;
 
+  // Cache the result globally for subsequent calls
+  globalWithMongoose.mongoose = cached;
+
   return cached.conn;
-}
+};
